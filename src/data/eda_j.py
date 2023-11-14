4de35853994519a138d4a014e3de1671fb2ab234
Ducampo <javier.ducampo@gmail.com>
2023-11-14 14:57:09 +0100

    update eda

--- src/data/eda_j.py
+++ src/data/eda_j.py
@@ -10,14 +10,26 @@ import matplotlib.pyplot as plt
 
 
 # Declare document variables, make sure to update relative paths to match local file location
-r_train_file = "D:/Tilburg University/Master Data Science/Block Two/Machine Learning/Group Assignment/Data Files/train.json"
-r_test_file = "D:/Tilburg University/Master Data Science/Block Two/Machine Learning/Group Assignment/Data Files/test.json"
+r_train_file = "add path to folder where you have stored the raw file"
+r_test_file = "add path to folder where you have stored the raw file"
 
 # raw data frames
 r_train_eda_df = pd.read_json(r_train_file)
-r_test_eda_df = pd.read_json(r_test_file)
+# r_test_eda_df = pd.read_json(r_test_file)
 
-# Imbalance distribution most of the data is from 2010 to 2020
+
+# Descriptive view training dataset
+r_train_eda_df.head()
+r_train_eda_df.shape
+r_train_eda_df.info()
+
+## To be checked: https://www.kaggle.com/code/harshsingh2209/complete-guide-to-eda-on-text-data
+r_train_eda_df.isnull().sum()
+r_train_eda_df["title"][65909]
+print(r_train_eda_df.iloc[65909])
+
+
+# imbalance distribution most of the data is from 2010 to 2020
 print(r_train_eda_df["year"])
 r_train_eda_df["year"].hist()
 plt.show()
